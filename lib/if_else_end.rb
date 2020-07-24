d727881c592668db71da43743104c7dec71940c8
Maysoon Tabali <mtab03@me.com>
2020-07-24 17:42:14 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,17 +2,17 @@
 current_time = Time.now 
 current_time = current_time.to_i 
 
-if current_time % 2 == 0 
-    puts "Even!"
-  else 
-    puts "Odd!"
-  end
+# if current_time % 2 == 0 
+#     puts "Even!"
+#   else 
+#     puts "Odd!"
+#   end
   
 current_time = Time.now
 current_time = current_time.to_i
  
-if current_time.even?
-  puts "Even!"
-else
-  puts "Odd!"
-end
+# if current_time.even?
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
